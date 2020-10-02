fe0c8a1f0a69faa8336601f00b0cd6acbcab8fcb
lna <lna.andprad@gmail.com>
2020-10-02 18:27:48 +0100

    changes

--- todo/admin.py
+++ todo/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Todo # add this
 
 class TodoAdmin(admin.ModelAdmin):  # add this
-    list_display = ('user', 'content', 'completed') # add this
+    list_display = ('__str__','user', 'content', 'completed') # add this
     search_fieds = ['user__username', 'user_email']
     class Meta:
         model = Todo
