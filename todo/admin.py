de73e418443d9206470a4c3ec73fadd52aeb891a
lna <lna.andprad@gmail.com>
2020-10-04 12:24:14 +0100

    changes

--- todo/admin.py
+++ todo/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Todo # add this
+from .models import Todo, TodoAssign
 
-class TodoAdmin(admin.ModelAdmin):  # add this
+class TodoAssignAdmin(admin.TabularInline):
+    model = TodoAssign
+
+
+class TodoAdmin(admin.ModelAdmin):
+    inlines = [TodoAssignAdmin]
     list_display = ('__str__','user', 'content', 'completed') # add this
     search_fieds = ['user__username', 'user_email']
     class Meta:
--- todo/models.py
+++ todo/models.py
@@ -6,8 +6,10 @@ User = settings.AUTH_USER_MODEL
 class Todo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.TextField(blank=True, null=True)
+    assign = models.ManyToManyField(User, related_name='todo_user', blank=True, through='TodoAssign')
     content = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-id']
@@ -23,3 +25,8 @@ class Todo(models.Model):
             "title": self.title,
             "content":self.content
         }
+
+class TodoAssign(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    todo = models.ForeignKey(Todo, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
